b08992b9a00d80cd437cf3f6540319d88ea6cd98
AnthonyZalev <40033720+AnthonyZalev@users.noreply.github.com>
2019-05-02 10:48:48 -0400

    Update DataBaseReadWrite.py

--- DataBaseReadWrite.py
+++ DataBaseReadWrite.py
@@ -1,9 +1,17 @@
 import json
+class DataBase:
 
-def  WriteToJSONFIle(path,filename,data):
-    filePathNameWithExt = "./" + path + "/" + filename + ".json"
-    with open(filePathNameWithExt,"w") as fp:
-        json.dump(data, fp)
+    def __init__(self):
+        self.path = "./"
+        self.filename = "AcidNameKADAtaBase.json"
 
-path = "./"
-filename = "AcidNameKADAtaBase.json"
+    def WriteToJSONFIle(self, data):
+        filePathNameWithExt = "./" + self.path + "/" + self.filename
+        with open(filePathNameWithExt,"w") as fp:
+            json.dump(data, fp)
+
+    def ReadJSONFile(self):
+        with open(self.filename) as json_file:
+            data = json.load(json_file)
+            for p in data:
+               pr
